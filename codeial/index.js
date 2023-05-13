cb2aaba60e3188502eac807ba5b1414fd08e226e
Kartik <Kartik>
2023-05-13 20:54:37 +0530

    initial commit

---
+++ codeial/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.listen(port,function(error){
+ if(error){
+    console.log(`Error is running the server:  ${error}`);
+ }
+    console.log(`Server is running on port:  ${port}`);
+});
